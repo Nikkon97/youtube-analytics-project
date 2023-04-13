8f4abd87eddee5682e8038f76b783b73dcf4e123
Nikkon97 <you@example.com>
2023-04-13 17:23:19 +0300

    homework 2

--- src/channel.py
+++ src/channel.py
@@ -1,8 +1,9 @@
-import json
 import os
+import json
 from googleapiclient.discovery import build
 
-import isodate
+
+api_key = os.getenv('API_KEY')
 
 
 class Channel:
@@ -10,11 +11,50 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.api_key = os.getenv('API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.__channel_id: str = channel_id
+        response = self.get_info()
+        self.title: str = response['items'][0]['snippet']['title']
+        self.description: str = response['items'][0]['snippet']['description']
+        self.url: str = 'https://www.youtube.com/channel/' + channel_id
+        self.subscribers: int = response['items'][0]['statistics']['subscriberCount']
+        self.video_count: int = response['items'][0]['statistics']['videoCount']
+        self.view_count: int = response['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        response = self.get_info()
+        print(json.dumps(response, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """
+        Возвращает объект для работы с YouTube API
+        """
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename: str) -> None:
+        """
+        Сохраняет в файл значения атрибутов экземпляра Channel
+        """
+        data = dict(map(lambda i: (i[0].removeprefix('_Channel__'), i[1]), self.__dict__.items()))
+        with open(filename, 'w') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+
+    def get_info(self) -> dict:
+        """
+        Получает информацию о канале
+        """
+        response = self.get_service().channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
+        return response
+
+    @channel_id.setter
+    def channel_id(self, value):
+        # self._channel_id = value
+        print("AttributeError: property 'channel_id' of 'Channel' object has no setter")
